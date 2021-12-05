d99530a71637605d9d0c76a1d1fe1e5c2d365d72
BmF10 <bimafebriansyah1002@gmail.com>
2021-12-05 10:55:51 +0700

    change to typescript & init util

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+
+const App: FC = () => {
+  return (
+    <div className="bg-gray-100 h-screen w-full flex justify-center content-center items-center flex-col">
+      <h1 className="text-7xl font-bold">Fun & Action</h1>
+      <h2 className="text-4xl mt-3">We will release soon</h2>
+      <h3 className="text-3xl mt-3">Stay tuned!</h3>
+    </div>
+  )
+}
+
+export default App
